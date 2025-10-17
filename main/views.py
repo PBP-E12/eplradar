714310ffdd92b5bc6b6686544e4d165721321c40
raihanheriandry <raihanmaulana.heriandry@gmail.com>
2025-10-17 23:05:06 +0700

    add main template

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    context = {
+        'title': 'EPLRadar'
+    }
+
+    return render(request, "main.html", context)
