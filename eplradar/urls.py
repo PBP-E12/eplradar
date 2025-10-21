ef8f0bc1800b252970dc215216f79d9ea19fa8d7
antoniusdaniel06 <danilnainggolan10@gmail.com>
2025-10-21 19:53:38 +0700

    menghapus comment pada urls di project

--- eplradar/urls.py
+++ eplradar/urls.py
@@ -19,10 +19,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('main.urls')), 
-    # path('clubs/', include('clubs.urls')),
-    # path('players/', include('players.urls')),
-    # path('stats/', include('stats.urls')),
+    path('', include('main.urls')), 
+    path('clubs/', include('clubs.urls')),
+    path('players/', include('players.urls')),
+    path('stats/', include('stats.urls')),
     path('news/', include('news.urls')),
-    # path('matches/', include('matches.urls')),
+    path('matches/', include('matches.urls')),
 ]
