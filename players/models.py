f444d2d7726977a05796c64ac7b0cdad8e3c7f27
Ahmad Zein Rasyid <ahmad.zein41@ui.ac.id>
2025-10-22 14:09:43 +0700

    initial player migrations

--- players/models.py
+++ players/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class Player(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     position = models.CharField(max_length=20)
     team = models.CharField(max_length=100)
-    profile_picture = models.ImageField(upload_to='player_pics/', null=True, blank=True)
+    profile_picture_url = models.URLField(blank=True, null=True)
     citizenship = models.CharField(max_length=50)
     age = models.IntegerField()
     curr_goals = models.PositiveIntegerField()
