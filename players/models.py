d80129b62be0179528d73f125e84c0c2b2cb8a23
Ahmad Zein Rasyid <ahmad.zein41@ui.ac.id>
2025-10-22 13:41:27 +0700

    Changed players model to reflect figma

--- players/models.py
+++ players/models.py
@@ -4,10 +4,13 @@ from django.db import models
 class Player(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    position = models.CharField(max_length=50)
+    position = models.CharField(max_length=20)
     team = models.CharField(max_length=100)
     profile_picture = models.ImageField(upload_to='player_pics/', null=True, blank=True)
+    citizenship = models.CharField(max_length=50)
     age = models.IntegerField()
+    curr_goals = models.PositiveIntegerField()
+
 
     def __str__(self):
         return self.name
