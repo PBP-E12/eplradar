8a57ec4753f885c38865fb2ce9eb453de893df26
antoniusdaniel06 <danilnainggolan10@gmail.com>
2025-10-23 02:18:15 +0700

    menambahkan fitur add berita menggunakan ajax

--- news/urls.py
+++ news/urls.py
@@ -6,4 +6,6 @@ app_name = 'news'
 urlpatterns = [
     path('', views.news_list, name='news_list'),
     path('<int:pk>/', views.news_detail, name='news_detail'),
+    path('add-news-ajax/', views.add_news_ajax, name='add_news_entry_ajax'),
+
 ]
