e7e65a877b8452ba1178a1e95a0a471c5db31f4a
raihanheriandry <raihanmaulana.heriandry@gmail.com>
2025-10-22 13:34:20 +0700

    add navbar and home

--- players/models.py
+++ players/models.py
@@ -6,7 +6,7 @@ class Player(models.Model):
     name = models.CharField(max_length=100)
     position = models.CharField(max_length=50)
     team = models.CharField(max_length=100)
-    profile_picture = models.ImageField(upload_to='player_pics/', null=True, blank=True)
+    # profile_picture = models.ImageField(upload_to='player_pics/', null=True, blank=True)
     age = models.IntegerField()
 
     def __str__(self):
