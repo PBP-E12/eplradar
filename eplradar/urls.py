c15586e751ef4553d92447d161305679d95290d8
antoniusdaniel06 <danilnainggolan10@gmail.com>
2025-10-24 00:31:57 +0700

    add delete and update with ajax

--- eplradar/urls.py
+++ eplradar/urls.py
@@ -20,9 +20,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')), 
-    # path('clubs/', include('clubs.urls')),
-    # path('players/', include('players.urls')),
-    # path('stats/', include('stats.urls')),
+    path('clubs/', include('clubs.urls')),
+    path('players/', include('players.urls')),
+    path('stats/', include('stats.urls')),
     path('news/', include('news.urls')),
-    #path('matches/', include('matches.urls')),
+    path('matches/', include('matches.urls')),
 ]
