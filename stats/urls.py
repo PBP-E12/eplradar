aa2f5328371065375bbb3789e0f3daf332835b45
raihanheriandry <raihanmaulana.heriandry@gmail.com>
2025-10-24 00:32:28 +0700

    add card stats

--- stats/urls.py
+++ stats/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from stats.views import show_stats, player_stats, club_stats
+from stats.views import show_stats
+# player_stats, club_stats
 
 app_name = 'stats'
 
 urlpatterns = [
     path('', show_stats, name='show_stats'),
-    path('api/stats/players/', player_stats, name='player_stats'),
-    path('api/stats/clubs/', club_stats, name='club_stats'),
+    # path('api/stats/players/', player_stats, name='player_stats'),
+    # path('api/stats/clubs/', club_stats, name='club_stats'),
 ]
