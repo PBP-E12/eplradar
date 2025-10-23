c5b1478947400956f790bfb07f355b702a934798
Ahmad Zein Rasyid <ahmad.zein41@ui.ac.id>
2025-10-24 00:55:37 +0700

    url mapping

--- players/urls.py
+++ players/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from views import show_player_detail, show_player_main
 
 urlpatterns = [
-    path('/players/<uuid:id>', show_player_detail, name='show_player_detail'),
-    path('/players', show_player_main, name='show_player_main'),
+    path('/<uuid:id>', show_player_detail, name='show_player_detail'),
+    path('', show_player_main, name='show_player_main'),
 ]
