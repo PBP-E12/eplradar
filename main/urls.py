b6eafc0b7785565cb0733eabca35ef1dbeab0da1
antoniusdaniel06 <danilnainggolan10@gmail.com>
2025-10-23 11:59:21 +0700

    fix login logout register

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,6 @@ app_name = 'main'
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('register/', register, name='register'),
-    path('login/', login_user, name='login_user'),
-    path('logout/', logout_user, name='logout_user'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
