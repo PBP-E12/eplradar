5a30dffa8e4b73ee3579adc4c0ad61c0ee4322e2
raihanheriandry <raihanmaulana.heriandry@gmail.com>
2025-10-23 23:52:41 +0700

    ubah stats

--- eplradar/urls.py
+++ eplradar/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('', include('main.urls')), 
     # path('clubs/', include('clubs.urls')),
     # path('players/', include('players.urls')),
-    #path('stats/', include('stats.urls')),
+    path('stats/', include('stats.urls')),
     path('news/', include('news.urls')),
-    #path('matches/', include('matches.urls')),
+    path('matches/', include('matches.urls')),
 ]
--- stats/urls.py
+++ stats/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from stats.views import show_stats
+from stats.views import show_stats, player_stats, club_stats
 
 app_name = 'stats'
 
 urlpatterns = [
     path('', show_stats, name='show_stats'),
+    path('api/stats/players/', player_stats, name='player_stats'),
+    path('api/stats/clubs/', club_stats, name='club_stats'),
 ]
