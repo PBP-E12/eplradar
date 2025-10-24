b371a7dc3caeb1798879b31ba1ffa2d986a81e59
raihanheriandry <raihanmaulana.heriandry@gmail.com>
2025-10-25 00:58:18 +0700

    add player

--- stats/urls.py
+++ stats/urls.py
@@ -6,5 +6,5 @@ app_name = 'stats'
 urlpatterns = [
     path('', views.show_stats, name='show_stats'),
     path('api/stats/clubs/', views.club_stats_api, name='club_stats_api'),
-    path('api/stats/players/', views.player_stats_api, ...)
+    path('api/stats/players/', views.player_stats_api, name='player_stats_api'),
 ]
