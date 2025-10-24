09265559c81db5e8145b99450b1a6d653f520f33
Ahmad Zein Rasyid <ahmad.zein41@ui.ac.id>
2025-10-24 11:04:00 +0700

    almost finished implementation of html

--- players/views.py
+++ players/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Player
+from clubs.models import Club
 
 def show_player_detail(request, id=0):
     player = get_object_or_404(Player, id=id)
@@ -9,8 +10,25 @@ def show_player_detail(request, id=0):
     return render(request, 'playerprofilepage.html', context)
 
 def show_player_main(request):
-    players = Player.objects.all()
+    team_id = request.GET.get('team')
+    
+    # Filter players by team if specified
+    if team_id and team_id != 'all':
+        players = Player.objects.filter(team_id=team_id)
+    else:
+        players = Player.objects.all()
+    
+    # Get all clubs for the filter dropdown
+    clubs = Club.objects.all()
+    
     context = {
-        'player_list' : players
+        'player_list': players,
+        'clubs': clubs
     }
+    
+    # If it's an AJAX request, return only the player cards
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        return render(request, 'player_cards.html', context)
+    
+    # Otherwise return the full page
     return render(request, 'playerspage.html', context)
