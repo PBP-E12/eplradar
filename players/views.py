b486a74ec4292728ccf70e1fe947576a8d9bc3d1
Ahmad Zein Rasyid <ahmad.zein41@ui.ac.id>
2025-10-24 15:38:22 +0700

    updated players

--- players/views.py
+++ players/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
 from .models import Player
 from clubs.models import Club
 
-def show_player_detail(request, id=0):
+def show_player_detail(request, id):
     player = get_object_or_404(Player, id=id)
+    
+    # If it's an AJAX request, return JSON
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        data = {
+            'id': str(player.id),
+            'name': player.name,
+            'position': player.position,
+            'team_name': player.team.nama_klub,
+            'profile_picture_url': player.profile_picture_url or '',
+            'citizenship': player.citizenship,
+            'age': player.age,
+            'curr_goals': player.curr_goals,
+            'curr_assists': player.curr_assists,
+            'match_played': player.match_played,
+            'curr_cleansheet': player.curr_cleansheet,
+        }
+        return JsonResponse(data)
+    
+    # Otherwise return the full page (if you still need it)
     context = {
         'player': player
     }
