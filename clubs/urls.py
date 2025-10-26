3a84ca9ad8d3329f969de9e5a34b91f7a6b6ffd8
omarsuyuf <omarsuyufw@gmail.com>
2025-10-26 23:25:18 +0700

    buat komentar dan filter untuk user login

--- clubs/urls.py
+++ clubs/urls.py
@@ -6,5 +6,11 @@ app_name = 'clubs'
 urlpatterns = [
     path('', views.club_list, name='club_list'),
     path('api/clubs/', views.club_list_api, name='club_list_api'),
+    
+    path('api/comments/', views.get_comments_api, name='get_comments_api'),
+    path('api/comments/create/', views.create_comment_api, name='create_comment_api'),
+    path('api/comments/<int:comment_id>/update/', views.update_comment_api, name='update_comment_api'),
+    path('api/comments/<int:comment_id>/delete/', views.delete_comment_api, name='delete_comment_api'),
+    
     path('<str:nama_klub>/', views.club_detail, name='club_detail'),
 ]
