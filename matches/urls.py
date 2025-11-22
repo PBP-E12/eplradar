d5a32e2e4efe82785e521d285449b687f810c121
OscarGlad14 <oscarsimanullang141@gmail.com>
2025-11-23 01:29:42 +0700

    Add CRUD for matches module

--- matches/urls.py
+++ matches/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import show_matches
+from .views import show_matches, add_prediction_ajax, update_prediction_ajax, delete_prediction_ajax
 
 app_name = 'matches'
 
 urlpatterns = [
     path('', show_matches, name='show_matches'),
+    path('add-prediction-ajax/', add_prediction_ajax, name='add_prediction_ajax'),
+    path('update-prediction-ajax/<int:prediction_id>/', update_prediction_ajax, name='update_prediction_ajax'),
+    path('delete-prediction-ajax/<int:prediction_id>/', delete_prediction_ajax, name='delete_prediction_ajax'),
 ]
