4d3dd9c677984ffec9d68810007f7fcb3cf25554
OscarGlad14 <oscarsimanullang141@gmail.com>
2025-12-10 22:14:17 +0700

    Fix API match and klasemen

--- matches/urls.py
+++ matches/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('add-prediction-ajax/', add_prediction_ajax, name='add_prediction_ajax'),
     path('update-prediction-ajax/<int:prediction_id>/', update_prediction_ajax, name='update_prediction_ajax'),
     path('delete-prediction-ajax/<int:prediction_id>/', delete_prediction_ajax, name='delete_prediction_ajax'),
-    path('show_klasemen_api/', show_klasemen_api, name='show_klasemen_api' ),
-    path('show_matches_api/', show_matches_api, name='show_matches_api'),
+    path('show-klasemen-api/', show_klasemen_api, name='show_klasemen_api' ),
+    path('show-matches-api/', show_matches_api, name='show_matches_api'),
     path('json-match/', show_json_match, name='show_json'),
     path('json-prediction/', show_json_prediction, name='show_json_prediction'),
 ]
