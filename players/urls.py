81017e6cf775aa36f65683184dce85e44a6fd2d0
Ahmad Zein Rasyid <ahmad.zein41@ui.ac.id>
2025-12-21 19:45:47 +0700

    Added comment CRUD for players

--- players/urls.py
+++ players/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import show_player_detail, show_player_main, api_players, api_players_delete, api_player_image
+from .views import show_player_detail, show_player_main, api_players, api_players_delete, api_player_image, api_player_comments
 
 app_name = 'players'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('api/', api_players, name='api_players'),
     path('api/delete/', api_players_delete, name='api_players_delete'),
     path('api/image/<uuid:player_id>/', api_player_image, name='api_player_image'),
+    path('<uuid:player_id>/comments/', api_player_comments, name='api_player_comments'),
 ]
